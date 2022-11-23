2fc526f74d20e28a5001bd2503703b197f22b48f
hodongtu <32997931+hodongtu@users.noreply.github.com>
2022-11-23 12:43:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Thai'}
+    return render_template('index.html', title='Home', user=user)
