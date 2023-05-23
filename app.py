bb1f27f9235acb95abbea8051be0a9b69cae8bd1
hodongtu <32997931+hodongtu@users.noreply.github.com>
2023-05-23 11:57:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+"""
+    :author: tuhd2
+    :license: MIT, see LICENSE for more details.
+"""
+import os
+from flask import Flask, render_template, flash, redirect, url_for
+
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'secret string tuhd2')
 
 @app.route('/')
-@app.route('/index')
 def index():
-    user = {'username': 'Thai'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html')
+
+@app.route('/hpbd')
+def hpbd():
+    return render_template('hpbd.html')
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
